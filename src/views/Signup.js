353d1defd9904230558bd7989d429fb9096957f1
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-01-18 20:13:11 +0300

    :boom: Connect front to backend

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
+import axiosClient from '../client'
+import { useStateContext } from '../context/Provider'
 
 function Signup() {
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passRef = useRef()
+  const passConfirmationRef = useRef()
+
+  const {setToken, setUser} = useStateContext()
+
     const onSubmit = (e) => {
       e.preventDefault()
+      const payload = {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        password: passRef.current.value,
+        password_confirmation: passConfirmationRef.current.value
+      }
+
+      axiosClient.post('/signup', payload)
+        .then(({data}) => {
+          setUser(data.user)
+          setToken(data.token)
+        })
+        .catch(err => {
+          const res = err.response
+          if(res && res.status === 422) {
+            console.log(res.data.errors)
+          }
+        })
     }
   
     return (
@@ -13,10 +40,10 @@ function Signup() {
             <h1 className='title'>
               Signup for free 
             </h1>
-            <input type='text' placeholder='Full Name'/>
-            <input type='email' placeholder='Email Address'/>
-            <input type='password' placeholder='Password'/>
-            <input type='password' placeholder='Password Confirmation'/>
+            <input ref={nameRef} type='text' placeholder='Full Name'/>
+            <input ref={emailRef} type='email' placeholder='Email Address'/>
+            <input ref={passRef} type='password' placeholder='Password'/>
+            <input ref={passConfirmationRef} type='password' placeholder='Password Confirmation'/>
             <button className='btn btn-block'>Signup</button>
             <p className='message'>
               Already Registered? <Link to='/login'>Login</Link>
